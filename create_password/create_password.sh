85c9e667f80b424305caa4fc593813a5bfe9fb90
tomato-tom <hajimeee49@gmail.com>
2023-11-21 23:25:57 +0900

    パスワード生成スクリプト

--- create_password/create_password.sh
+++ create_password/create_password.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+# １０文字のパスワードを生成して標準出力します
 
-# Generate a random password with at least one uppercase letter, one lowercase letter, and one digit
-PASSWORD=$(</dev/urandom tr -dc 'A-Za-z0-9' | fold -w 10 | grep -E '.*[A-Z].*[a-z].*[0-9].*' | head -n 1)
-#PASSWORD=$(</dev/urandom tr -dc 'A-Za-z0-9' | fold -w $1 | grep -E '.*[A-Z].*[a-z].*[0-9].*' | head -n 1)
+password_chars=""
+regex=""
+
+# オプションを解析
+while getopts "dlu" opt; do
+  case $opt in
+    d)
+      password_chars+="0-9"
+      regex+=".*[0-9]"
+      ;;
+    l)
+      password_chars+="a-z"
+      regex+=".*[a-z]"
+      ;;
+    u)
+      password_chars+="A-Z"
+      regex+=".*[A-Z]"
+      ;;
+    \?)
+      echo "Usage: $0 [-d] [-l] [-u]"
+      exit 1
+      ;;
+  esac
+done
+
+# オプションがない場合はデフォルト、大文字、小文字、数字が含まれます
+if [ -z "$regex" ]; then
+  password_chars+="0-9a-zA-Z"
+  regex=".*[A-Z].*[a-z].*[0-9].*"
+fi
+
+# urandomによりパスワード生成
+PASSWORD=$(</dev/urandom tr -dc "$password_chars" | fold -w 10 | grep -E "$regex" | head -n 1)
 
 echo "${PASSWORD}"
+
+# パスワードをクリップボードに渡す場合
 #echo "${PASSWORD}" | xsel -bi
